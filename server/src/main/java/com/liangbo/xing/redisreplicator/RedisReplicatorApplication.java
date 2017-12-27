7d0988b05e198341c79c38b28ac9aef7b51219d7
xingliangbo <xingliangbo@u51.com>
2017-12-27 10:09:50 +0800

    init project

---
+++ server/src/main/java/com/liangbo/xing/redisreplicator/RedisReplicatorApplication.java
@@ -0,0 +1,12 @@
+package com.liangbo.xing.redisreplicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisReplicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisReplicatorApplication.class, args);
+	}
+}
